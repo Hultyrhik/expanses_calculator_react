87b0d545eea530294c0c84af170a3c988f06b32b
Fyodor Bashlakov <feiaoduoit@gmail.com>
2024-09-29 15:54:30 +0700

    delete unused code and add clearing to number input

--- vite-project/src/components/ExpenseListItem.jsx
+++ vite-project/src/components/ExpenseListItem.jsx
@@ -2,14 +2,14 @@ import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import Avatar from "@mui/material/Avatar";
-import ImageIcon from "@mui/icons-material/Image";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 
 export default function ExpenseListItem({ item }) {
   return (
     <ListItem>
       <ListItemAvatar>
         <Avatar>
-          <ImageIcon />
+          <DeleteForeverIcon />
         </Avatar>
       </ListItemAvatar>
       <ListItemText
