bcbae6be4bbd9031e1dd426fc2c4b329f7b9fc88
Fyodor Bashlakov <feiaoduoit@gmail.com>
2024-09-29 15:45:47 +0700

    fix number input to prevent char input

--- vite-project/src/components/ExpenseForm.jsx
+++ vite-project/src/components/ExpenseForm.jsx
@@ -6,9 +6,39 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
+import {
+  Unstable_NumberInput as BaseNumberInput,
+  numberInputClasses,
+} from "@mui/base";
+import { styled } from "@mui/material/styles";
+import { NumericFormat } from "react-number-format";
+
+const NumberInput = React.forwardRef(function CustomNumberInput(props, ref) {
+  return (
+    <BaseNumberInput
+      slots={{
+        root: StyledInputRoot,
+        input: StyledInputElement,
+        incrementButton: StyledButton,
+        decrementButton: StyledButton,
+      }}
+      slotProps={{
+        incrementButton: {
+          children: "▴",
+        },
+        decrementButton: {
+          children: "▾",
+        },
+      }}
+      {...props}
+      ref={ref}
+    />
+  );
+});
 
 export default function ExpenseForm({ AddListItem }) {
   const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState(0);
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -33,12 +63,11 @@ export default function ExpenseForm({ AddListItem }) {
             const formData = new FormData(event.currentTarget);
             const formJson = Object.fromEntries(formData.entries());
             const category = formJson.category;
-            const sum = formJson.sum;
             const date = formJson.date;
             const description = formJson.description;
             const item = {
               category: category,
-              sum: sum,
+              sum: value,
               date: date,
               description: description,
             };
@@ -52,7 +81,6 @@ export default function ExpenseForm({ AddListItem }) {
         <DialogContent>
           <DialogContentText>Please add item</DialogContentText>
           <TextField
-            autoFocus
             required
             margin="dense"
             id="category"
@@ -62,8 +90,7 @@ export default function ExpenseForm({ AddListItem }) {
             fullWidth
             variant="standard"
           />
-          <TextField
-            autoFocus
+          {/* <TextField
             required
             margin="dense"
             id="sum"
@@ -72,9 +99,32 @@ export default function ExpenseForm({ AddListItem }) {
             type="text"
             fullWidth
             variant="standard"
+            min="0"
+            placeholder="0"
+            
+          /> */}
+          {/* <NumberInput
+            required
+            label="sum"
+            aria-label="Demo number input"
+            placeholder="Type a number…"
+            value={value}
+            onChange={(event, val) => {
+              setValue(val);
+              console.log("VAL", val);
+              console.log("VALUE", value);
+            }}
+            onInputChange={(event) => setValue(event.target.value)}
+            min={0}
+          /> */}
+          <NumericFormat
+            value={value}
+            prefix="$"
+            onValueChange={(values) => {
+              setValue(values.value);
+            }}
           />
           <TextField
-            autoFocus
             required
             margin="dense"
             id="date"
@@ -84,7 +134,6 @@ export default function ExpenseForm({ AddListItem }) {
             variant="standard"
           />
           <TextField
-            autoFocus
             required
             margin="dense"
             id="description"
@@ -103,3 +152,144 @@ export default function ExpenseForm({ AddListItem }) {
     </React.Fragment>
   );
 }
+
+const blue = {
+  100: "#DAECFF",
+  200: "#80BFFF",
+  400: "#3399FF",
+  500: "#007FFF",
+  600: "#0072E5",
+};
+
+const grey = {
+  50: "#F3F6F9",
+  100: "#E5EAF2",
+  200: "#DAE2ED",
+  300: "#C7D0DD",
+  400: "#B0B8C4",
+  500: "#9DA8B7",
+  600: "#6B7A90",
+  700: "#434D5B",
+  800: "#303740",
+  900: "#1C2025",
+};
+
+const StyledInputRoot = styled("div")(
+  ({ theme }) => `
+    font-family: 'IBM Plex Sans', sans-serif;
+    font-weight: 400;
+    border-radius: 8px;
+    color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
+    background: ${theme.palette.mode === "dark" ? grey[900] : "#fff"};
+    border: 1px solid ${theme.palette.mode === "dark" ? grey[700] : grey[200]};
+    box-shadow: 0px 2px 2px ${
+      theme.palette.mode === "dark" ? grey[900] : grey[50]
+    };
+    display: grid;
+    grid-template-columns: 1fr 19px;
+    grid-template-rows: 1fr 1fr;
+    overflow: hidden;
+    column-gap: 8px;
+    padding: 4px;
+  
+    &.${numberInputClasses.focused} {
+      border-color: ${blue[400]};
+      box-shadow: 0 0 0 3px ${
+        theme.palette.mode === "dark" ? blue[600] : blue[200]
+      };
+    }
+  
+    &:hover {
+      border-color: ${blue[400]};
+    }
+  
+    // firefox
+    &:focus-visible {
+      outline: 0;
+    }
+  `
+);
+
+const StyledInputElement = styled("input")(
+  ({ theme }) => `
+    font-size: 0.875rem;
+    font-family: inherit;
+    font-weight: 400;
+    line-height: 1.5;
+    grid-column: 1/2;
+    grid-row: 1/3;
+    color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
+    background: inherit;
+    border: none;
+    border-radius: inherit;
+    padding: 8px 12px;
+    outline: 0;
+  `
+);
+
+const StyledButton = styled("button")(
+  ({ theme }) => `
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: center;
+    align-items: center;
+    appearance: none;
+    padding: 0;
+    width: 19px;
+    height: 19px;
+    font-family: system-ui, sans-serif;
+    font-size: 0.875rem;
+    line-height: 1;
+    box-sizing: border-box;
+    background: ${theme.palette.mode === "dark" ? grey[900] : "#fff"};
+    border: 0;
+    color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
+    transition-property: all;
+    transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+    transition-duration: 120ms;
+  
+    &:hover {
+      background: ${theme.palette.mode === "dark" ? grey[800] : grey[50]};
+      border-color: ${theme.palette.mode === "dark" ? grey[600] : grey[300]};
+      cursor: pointer;
+    }
+  
+    &.${numberInputClasses.incrementButton} {
+      grid-column: 2/3;
+      grid-row: 1/2;
+      border-top-left-radius: 4px;
+      border-top-right-radius: 4px;
+      border: 1px solid;
+      border-bottom: 0;
+      &:hover {
+        cursor: pointer;
+        background: ${blue[400]};
+        color: ${grey[50]};
+      }
+  
+    border-color: ${theme.palette.mode === "dark" ? grey[800] : grey[200]};
+    background: ${theme.palette.mode === "dark" ? grey[900] : grey[50]};
+    color: ${theme.palette.mode === "dark" ? grey[200] : grey[900]};
+    }
+  
+    &.${numberInputClasses.decrementButton} {
+      grid-column: 2/3;
+      grid-row: 2/3;
+      border-bottom-left-radius: 4px;
+      border-bottom-right-radius: 4px;
+      border: 1px solid;
+      &:hover {
+        cursor: pointer;
+        background: ${blue[400]};
+        color: ${grey[50]};
+      }
+  
+    border-color: ${theme.palette.mode === "dark" ? grey[800] : grey[200]};
+    background: ${theme.palette.mode === "dark" ? grey[900] : grey[50]};
+    color: ${theme.palette.mode === "dark" ? grey[200] : grey[900]};
+    }
+    & .arrow {
+      transform: translateY(-1px);
+    }
+  `
+);
